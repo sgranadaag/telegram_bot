fe2ed13310ec045d1b2a6c4a20cf9c85c101655a
sgranadaag <santiagogranadaaguirre@gmail.com>
2023-01-21 14:28:24 -0500

    feat: add bot with persistence

---
+++ bot.py
@@ -0,0 +1,99 @@
+from config import bot
+import config
+from time import sleep
+import re
+import logic
+import database.db as db
+
+if __name__ == '__main__':
+    db.Base.metadata.create_all(db.engine)
+
+
+@bot.message_handler(commands=['start'])
+def on_command_start(message):
+    bot.send_chat_action(message.chat.id, 'typing')
+    bot.send_message(
+        message.chat.id,
+        logic.get_welcome_message(bot.get_me()),
+        parse_mode="Markdown"
+    )
+    bot.send_message(
+        message.chat.id,
+        logic.get_help_message(),
+        parse_mode="Markdown"
+    )
+    logic.register_account(message.from_user.id)
+
+
+@bot.message_handler(commands=['help'])
+def on_command_help(message):
+    bot.send_chat_action(message.chat.id, 'typing')
+    bot.send_message(
+        message.chat.id,
+        logic.get_help_message(),
+        parse_mode="Markdown"
+    )
+
+
+@bot.message_handler(commands=['about'])
+def on_command_about(message):
+    bot.send_chat_action(message.chat.id, 'typing')
+    bot.send_message(
+        message.chat.id,
+        logic.get_about_this(config.VERSION),
+        parse_mode="Markdown"
+    )
+
+
+#########################################################
+@bot.message_handler(regexp=r"^(gane|gané|g) ([+-]?([0-9]*[.])?[0-9]+)$")
+def on_earn_money(message):
+
+    pass
+#########################################################
+
+
+@bot.message_handler(regexp=r"^(gaste|gasté|gg) ([+-]?([0-9]*[.])?[0-9]+)$")
+def on_spend_money(message):
+
+    pass
+#########################################################
+
+
+@bot.message_handler(regexp=r"^(listar ganancias|lg) en ([0-9]{1,2}) de ([0-9]{4})$")
+def on_list_earnings(message):
+    pass
+#########################################################
+
+
+@bot.message_handler(regexp=r"^(listar gastos|lgg) en ([0-9]{1,2}) de ([0-9]{4})$")
+def on_list_spendings(message):
+    pass
+#########################################################
+
+
+@bot.message_handler(regexp=r"^(obtener saldo|s)$")
+def on_get_balance(message):
+    bot.send_chat_action(message.chat.id, 'typing')
+    balance = logic.get_balance(message.from_user.id)
+    text = "\U0000274C Aún no tienes una cuenta asociada, ejecuta /start para arreglarlo."
+    if balance != None:
+        text = f"Tu saldo actual es ${balance}"
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(regexp=r"^(remover|r) (ganancia|g|gasto|gg) ([0-9]+)$")
+def on_remove_record(message):
+    pass
+#########################################################
+
+
+@bot.message_handler(func=lambda message: True)
+def on_fallback(message):
+    pass
+
+
+#########################################################
+if __name__ == '__main__':
+    bot.polling(timeout=20)
+#########################################################
---
+++ logic.py
@@ -0,0 +1,53 @@
+import database.db as db
+from models.account import Account
+from models.earning import Earning
+from models.spending import Spending
+from datetime import datetime
+from sqlalchemy import extract
+
+
+def get_about_this(VERSION):
+    response = (
+        f"Simple Expenses Bot (pyTelegramBot) v{VERSION}"
+        "\n\n"
+        "Desarrollado por Jorge I. Meza <jimezam@autonoma.edu.co>"
+    )
+    return response
+
+
+def get_help_message():
+    response = (
+        "Estos son los comandos y órdenes disponibles:\n"
+        "\n"
+        "*/start* - Inicia la interacción con el bot (obligatorio)\n"
+        "*/help* - Muestra este mensaje de ayuda\n"
+        "*/about* - Muestra detalles de esta aplicación\n"
+        "*gane|gané|g {cantidad}* - Registra un saldo positivo\n"
+        "*gaste|gasté|gg {cantidad}* - Registra un saldo negativo\n"
+        "*listar ganancias|lg en {índice_mes} de {año}* - Lista las ganancias de un mes/año\n"
+        "*listar gastos|lgg en {mes} de {año}* - Lista los gastos de un mes/año\n"
+        "*obtener saldo|s* - Muestra el saldo actual (disponible)\n"
+        "*remover|r ganancia|g|gasto|gg {índice}* - Remueve una ganancia o un gasto según su índice\n"
+        "*listar cuentas|lc* - Lista las cuentas registradas (sólo admin)\n"
+    )
+    return response
+
+
+def get_welcome_message(bot_data):
+    response = (
+        f"Hola, soy *{bot_data.first_name}* "
+        f"también conocido como *{bot_data.username}*.\n\n"
+        "¡Estoy aquí para ayudarte a registrar tus gastos!"
+    )
+    return response
+
+
+def register_account(user_id):
+    account = db.session.query(Account).get(user_id)
+    db.session.commit()
+    if account == None:
+        account = Account(user_id, 0)
+        db.session.add(account)
+        db.session.commit()
+        return True
+    return False
